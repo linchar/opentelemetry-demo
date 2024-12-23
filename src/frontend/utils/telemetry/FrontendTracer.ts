8d610c398b667ce6c150797a847805e7b7b533f1
linchar <linchar@gmail.com>
2024-12-23 08:45:40 -0700

    add getIP for client IP address export resoruce to be used by webVitial Instrumenation

--- src/frontend/utils/telemetry/FrontendTracer.ts
+++ src/frontend/utils/telemetry/FrontendTracer.ts
@@ -7,7 +7,7 @@ import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
 import { registerInstrumentations } from '@opentelemetry/instrumentation';
 import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
 import { Resource, browserDetector } from '@opentelemetry/resources';
-import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+import { SEMRESATTRS_SERVICE_INSTANCE_ID, SEMRESATTRS_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { SessionIdProcessor } from './SessionIdProcessor';
 import { detectResourcesSync } from '@opentelemetry/resources/build/src/detect-resources';
@@ -19,13 +19,30 @@ const {
   IS_SYNTHETIC_REQUEST = '',
 } = typeof window !== 'undefined' ? window.ENV : {};
 
-const FrontendTracer = () => {
-  let resource = new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: NEXT_PUBLIC_OTEL_SERVICE_NAME,
-  });
+// use public service to get ip address
+let cachedIP: string = '';
+async function getIP(): Promise<string> {
+  if (cachedIP != '') {
+    return cachedIP;
+  }
+  const response = await fetch('https://api.ipify.org?format=json');
+  const data = await response.json();
+  cachedIP = data.ip;
+  return cachedIP;
+}
 
-  const detectedResources = detectResourcesSync({ detectors: [browserDetector] });
-  resource = resource.merge(detectedResources);
+let resource = new Resource({
+  [SEMRESATTRS_SERVICE_NAME]: NEXT_PUBLIC_OTEL_SERVICE_NAME,
+  [SEMRESATTRS_SERVICE_INSTANCE_ID]:
+    typeof window !== 'undefined' ? await getIP() : "unknown ip",
+});
+
+const detectedResources = detectResourcesSync({ detectors: [browserDetector] });
+resource = resource.merge(detectedResources);
+// use resource in other files
+export const frontendResource = resource;
+
+const FrontendTracer = () => {
   const provider = new WebTracerProvider({ resource });
 
   provider.addSpanProcessor(new SessionIdProcessor());
