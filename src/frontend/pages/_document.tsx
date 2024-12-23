f7ff3431ae9ca3e55aeafbacaa3ca64c7d076a56
linchar <linchar@gmail.com>
2024-12-23 07:59:20 -0700

    add reference to use the public metrics endpoint

--- src/frontend/pages/_document.tsx
+++ src/frontend/pages/_document.tsx
@@ -3,9 +3,10 @@
 
 import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
-import {context, propagation} from "@opentelemetry/api";
+import { context, propagation } from "@opentelemetry/api";
 
-const { ENV_PLATFORM, WEB_OTEL_SERVICE_NAME, PUBLIC_OTEL_EXPORTER_OTLP_TRACES_ENDPOINT, OTEL_COLLECTOR_HOST} = process.env;
+const { ENV_PLATFORM, WEB_OTEL_SERVICE_NAME, PUBLIC_OTEL_EXPORTER_OTLP_TRACES_ENDPOINT,
+  PUBLIC_OTEL_EXPORTER_OTLP_METRICS_ENDPOINT, OTEL_COLLECTOR_HOST } = process.env;
 
 export default class MyDocument extends Document<{ envString: string }> {
   static async getInitialProps(ctx: DocumentContext) {
@@ -23,14 +24,19 @@ export default class MyDocument extends Document<{ envString: string }> {
       const isSyntheticRequest = baggage?.getEntry('synthetic_request')?.value === 'true';
 
       const otlpTracesEndpoint = isSyntheticRequest
-          ? `http://${OTEL_COLLECTOR_HOST}:4318/v1/traces`
-          : PUBLIC_OTEL_EXPORTER_OTLP_TRACES_ENDPOINT;
+        ? `http://${OTEL_COLLECTOR_HOST}:4318/v1/traces`
+        : PUBLIC_OTEL_EXPORTER_OTLP_TRACES_ENDPOINT;
+
+      const otlpMetricsEndpoint = isSyntheticRequest
+        ? `http://${OTEL_COLLECTOR_HOST}:4318/v1/metrics`
+        : PUBLIC_OTEL_EXPORTER_OTLP_METRICS_ENDPOINT;
 
       const envString = `
         window.ENV = {
           NEXT_PUBLIC_PLATFORM: '${ENV_PLATFORM}',
           NEXT_PUBLIC_OTEL_SERVICE_NAME: '${WEB_OTEL_SERVICE_NAME}',
           NEXT_PUBLIC_OTEL_EXPORTER_OTLP_TRACES_ENDPOINT: '${otlpTracesEndpoint}',
+          NEXT_PUBLIC_OTEL_EXPORTER_OTLP_METRICS_ENDPOINT: '${otlpMetricsEndpoint}',          
           IS_SYNTHETIC_REQUEST: '${isSyntheticRequest}',
         };`;
       return {
