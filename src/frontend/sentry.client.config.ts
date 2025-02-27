e0dc8f310612c5cb28ea158c33148270006dc2b7
linchar <linchar@gmail.com>
2025-02-27 08:38:09 -0700

    enable sentry sdk trace

--- src/frontend/sentry.client.config.ts
+++ src/frontend/sentry.client.config.ts
@@ -24,6 +24,10 @@ Sentry.init({
   // Define how likely Replay events are sampled when an error occurs.
   replaysOnErrorSampleRate: 1.0,
 
+  // We recommend adjusting this value in production, or using `tracesSampler`
+  // for finer control
+  tracesSampleRate: 1.0,
+
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 });
