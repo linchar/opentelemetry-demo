f48d3096b7eccfeae01f25c5956b9059c15e7a0f
linchar <linchar@gmail.com>
2025-03-25 10:14:23 -0600

    disable sentry sdk trace

--- src/frontend/sentry.client.config.ts
+++ src/frontend/sentry.client.config.ts
@@ -26,7 +26,8 @@ Sentry.init({
 
   // We recommend adjusting this value in production, or using `tracesSampler`
   // for finer control
-  tracesSampleRate: 1.0,
+  // disable sentry trace by commenting the following line
+  // tracesSampleRate: 1.0,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
